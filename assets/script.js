b403390141bcaacdb280b1d499fdd7e212bbe295
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-04-30 13:15:03 -0400

    almost there

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
-const topics = "HTML, CSS, Git, JavaScript";
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(let x = 0; x < topics.length; x++){
+  console.log(topics[x])
+}
+
+/*
+if (topics === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topics === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topics === 'Git') {
+  console.log("Let's study Git!");
+} else if (topics === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!')
+
+}
+*/
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
+
+const shape = [56, 47, 42, 33, 54]
+for(let i = 0; x < shape.length; x++) {
+  console.log(shape[i])
+}
